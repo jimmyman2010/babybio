c9ea3fbe9b40fda441b3704c46f2c89c7669a7d4
jimmyman2010 <tranduyminhman@gmail.com>
2017-03-07 22:40:05 +0700

    update working tree

--- wordpress/wp-content/themes/total/js/total-custom.js
+++ wordpress/wp-content/themes/total/js/total-custom.js
@@ -173,13 +173,13 @@ $('.ht-portfolio-cat-name-list').on( 'click', '.ht-portfolio-cat-name', function
     var elems = $container.isotope('getFilteredItemElements');
     
     elems.forEach(function(item, index){
-      if ( index == 0 || index == 4 ) {
-        $( item ).addClass('wide');
-        var bg = $(item).find('.ht-portfolio-image').attr('href');
-        $( item ).find('.ht-portfolio-wrap').css('background-image', 'url('+bg+')');
-      }else{
+      //if ( index == 0 || index == 4 ) {
+      //  $( item ).addClass('wide');
+      //  var bg = $(item).find('.ht-portfolio-image').attr('href');
+      //  $( item ).find('.ht-portfolio-wrap').css('background-image', 'url('+bg+')');
+      //}else{
         $( item ).removeClass('wide');
-      }
+      //}
     }); 
 
     GetMasonary();      
